1413872477ddcb73df9592302090f4a0bd551d4d
Ariel Burriss <ariburr@gmail.com>
2017-11-29 15:26:33 -0700

    update

---
+++ app/controllers/subs_controller.rb
@@ -0,0 +1,51 @@
+class SubsController < ApplicationController
+  before_action :find_sub, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @subs = Sub.all
+  end
+
+  def show
+  end
+
+  def new
+    @sub = Sub.new
+    render partial: 'form'
+  end
+
+  def edit
+    render partial: 'form'
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+    if @sub.save
+      redirect_to sub_path(@sub)
+    else
+      render partial: 'form'
+    end
+  end
+
+  def update
+    if @sub.update(sub_params)
+      redirect_to sub_path(@sub)
+    else
+      render partial: 'form'
+    end
+  end
+
+  def destroy
+    @sub.destroy
+    redirect_to subs_path
+  end
+
+  private
+    def find_sub
+      @sub = Sub.find(params[:id])
+    end
+
+    def sub_params
+      params.require(:sub).permit(:name)
+    end
+
+end
